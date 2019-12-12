a1b7d1e619cd86e2c920aca94a3178da33c182a2
Jukka Aho <ahojukka5@gmail.com>
2019-12-12 15:24:38 +0700

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Portfolio | Jukka Aho | @ahojukka5`,
+    description: `This is a portfolio web page done by myself for myself.`,
+    author: `@ahojukka5`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
