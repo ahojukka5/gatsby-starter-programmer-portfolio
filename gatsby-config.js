da8df705f82f8b98fe5c0e195d70a63be86c085d
Jukka Aho <ahojukka5@gmail.com>
2019-12-12 20:59:26 +0700

    Add possiblity to write posts using markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,33 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/posts`,
+      },
+    },
   ],
 }
---
+++ src/pages/posts.js
@@ -0,0 +1,51 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import styled from "styled-components"
+
+const Layout = styled.div``
+const Header = styled.h1`
+  display: inline-block;
+  border-bottom: 1px solid;
+`
+
+export default ({ data }) => {
+  return (
+    <Layout>
+      <div>
+        <Header>Blog posts</Header>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link to={node.fields.slug}>
+              <h3>
+                {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
