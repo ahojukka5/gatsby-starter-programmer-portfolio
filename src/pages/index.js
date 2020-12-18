201660e033ae293b5e7811dd0ff9422df2421531
Jukka Aho <ahojukka5@gmail.com>
2020-12-18 19:51:08 +0700

    Show blog posts on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 
-const IndexPage = () => {
-  return <Layout>Hello</Layout>;
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date
+        }
+      }
+    }
+  }
+`;
+
+const IndexPage = ({ data }) => {
+  return (
+    <>
+      <Layout>
+        {data.allMdx.nodes.map(({ excerpt, frontmatter }) => (
+          <>
+            <h1>{frontmatter.title}</h1>
+            <p>{frontmatter.date}</p>
+            <p>{excerpt}</p>
+          </>
+        ))}
+      </Layout>
+    </>
+  );
 };
 
 export default IndexPage;
