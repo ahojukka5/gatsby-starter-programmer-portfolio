f702ad7ea23780638371b7090d475933ae04e965
Jukka Aho <ahojukka5@gmail.com>
2020-12-18 19:40:41 +0700

    Style Layout (a bit)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import SEO from '../components/seo';
+import styled from 'styled-components';
 
 import useSiteMetadata from '../hooks/useSiteMetadata';
 import Header from './Header';
 import Footer from './Footer';
 
+const AppStyles = styled.main`
+  width: 800px;
+  margin: 0 auto;
+`;
+
 const Layout = ({ children }) => {
   const { title, description } = useSiteMetadata();
   return (
-    <>
+    <AppStyles>
       <SEO title="Jukka Aho" />
       <SEO title="ahojukka5" />
       <header>
@@ -18,7 +24,7 @@ const Layout = ({ children }) => {
       <footer>
         <Footer />
       </footer>
-    </>
+    </AppStyles>
   );
 };
 
