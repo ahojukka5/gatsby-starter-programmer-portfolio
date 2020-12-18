0bddce58cbcbe7b76f82f273367245409baf64a4
Jukka Aho <ahojukka5@gmail.com>
2020-12-18 23:36:41 +0700

    Move blog posts to posts.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,9 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
-import styled from 'styled-components';
 
 import Layout from '../components/Layout';
 
-const IndexWrapper = styled.main``;
-
-const PostWrapper = styled.div``;
-
-const IndexPage = ({ data }) => {
-  return (
-    <Layout>
-      <IndexWrapper>
-        {data.allMdx.nodes.map(({ id, excerpt, frontmatter, fields }) => (
-          <PostWrapper key={id}>
-            <Link to={fields.slug}>
-              <h1>{frontmatter.title}</h1>
-              <p>{frontmatter.date}</p>
-              <p>{excerpt}</p>
-            </Link>
-          </PostWrapper>
-        ))}
-      </IndexWrapper>
-    </Layout>
-  );
+const IndexPage = () => {
+  return <Layout>Hello, world! My name is Jukka.</Layout>;
 };
 
 export default IndexPage;
-
-export const query = graphql`
-  query SITE_INDEX_QUERY {
-    allMdx(
-      sort: { fields: [frontmatter___date], order: DESC }
-      filter: { frontmatter: { published: { eq: true } } }
-    ) {
-      nodes {
-        id
-        excerpt(pruneLength: 250)
-        frontmatter {
-          title
-          date
-        }
-        fields {
-          slug
-        }
-      }
-    }
-  }
-`;
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -2,29 +2,26 @@ import React from 'react';
 import { Link, graphql } from 'gatsby';
 import styled from 'styled-components';
 
-const Layout = styled.div``;
-const Header = styled.h1`
-  display: inline-block;
-  border-bottom: 1px solid;
-`;
+import Layout from '../components/Layout';
+
+const IndexWrapper = styled.main``;
+
+const PostWrapper = styled.div``;
 
 const Posts = ({ data }) => {
   return (
     <Layout>
-      <div>
-        <Header>Blog posts</Header>
-        <h4>{data.allMdx.totalCount} Posts</h4>
-        {data.allMdx.edges.map(({ node }) => (
-          <div key={node.id}>
-            <Link to={node.fields.slug}>
-              <h3>
-                {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
-              </h3>
-              <p>{node.excerpt}</p>
+      <IndexWrapper>
+        {data.allMdx.nodes.map(({ id, excerpt, frontmatter, fields }) => (
+          <PostWrapper key={id}>
+            <Link to={fields.slug}>
+              <h1>{frontmatter.title}</h1>
+              <p>{frontmatter.date}</p>
+              <p>{excerpt}</p>
             </Link>
-          </div>
+          </PostWrapper>
         ))}
-      </div>
+      </IndexWrapper>
     </Layout>
   );
 };
@@ -32,20 +29,20 @@ const Posts = ({ data }) => {
 export default Posts;
 
 export const query = graphql`
-  query {
-    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-          }
-          fields {
-            slug
-          }
-          excerpt
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date
+        }
+        fields {
+          slug
         }
       }
     }
