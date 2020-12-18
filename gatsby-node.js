5e040235612a700f5e83666a172f8119ce221696
Jukka Aho <ahojukka5@gmail.com>
2020-12-18 20:06:52 +0700

    Fix styles

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,20 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
+  const { createNodeField } = actions;
+  if (node.internal.type === 'Mdx' || node.internal.type === 'MarkdownRemark') {
+    const slug = createFilePath({ node, getNode });
     createNodeField({
+      name: 'slug',
       node,
-      name: `slug`,
       value: slug,
-    })
+    });
   }
-}
+};
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
   const result = await graphql(`
     query {
       allMarkdownRemark {
@@ -27,15 +27,15 @@ exports.createPages = async ({ graphql, actions }) => {
         }
       }
     }
-  `)
+  `);
 
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
-      component: path.resolve(`./src/templates/blog-post.js`),
+      component: path.resolve('./src/templates/blog-post.js'),
       context: {
         slug: node.fields.slug,
       },
-    })
-  })
-}
+    });
+  });
+};
