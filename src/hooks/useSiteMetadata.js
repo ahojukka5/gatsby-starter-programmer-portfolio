ef3a84a87751e71075fbb3b0e2c4c2b1b948ba56
Jukka Aho <ahojukka5@gmail.com>
2020-12-18 19:02:19 +0700

    Add useSiteMetadata hook

---
+++ src/hooks/useSiteMetadata.js
@@ -0,0 +1,17 @@
+import { graphql, useStaticQuery } from "gatsby"
+
+const query = graphql`
+  query SITE_METADATA_QUERY {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
+
+export const useSiteMetadata = () => {
+  const { site } = useStaticQuery(query)
+  return site.siteMetadata
+}
