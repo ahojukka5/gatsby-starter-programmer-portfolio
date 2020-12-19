c6b832b7e96bbfbb055c197648caa04e2df6730a
Jukka Aho <ahojukka5@gmail.com>
2020-12-19 23:43:30 +0700

    Add button to change color mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import { useColorMode } from 'theme-ui';
 
-const Header = ({ siteTitle, siteDescription }) => (
-  <Link to="/">
-    <h1>{siteTitle}</h1>
-    <p>{siteDescription}</p>
-  </Link>
-);
+const Header = ({ siteTitle, siteDescription }) => {
+  const [colorMode, setColorMode] = useColorMode();
+  return (
+    <header>
+      <button
+        onClick={e => {
+          setColorMode(colorMode === 'default' ? 'dark' : 'default');
+        }}
+      >
+        Toggle {colorMode === 'default' ? 'Dark' : 'Light'}
+      </button>
+      <Link to="/">
+        <h1>{siteTitle}</h1>
+        <p>{siteDescription}</p>
+      </Link>
+    </header>
+  );
+};
 
 export default Header;
