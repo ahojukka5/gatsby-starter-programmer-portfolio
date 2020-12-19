f95a8f986b4d366a2c1f2ecdadf328220d254b58
Jukka Aho <ahojukka5@gmail.com>
2020-12-19 13:51:33 +0700

    Add 'type' field, filter by type=post

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,43 +4,35 @@ const { createFilePath } = require('gatsby-source-filesystem');
 exports.onCreateNode = ({ node, getNode, actions: { createNodeField } }) => {
   const isMdx = node => node.internal.type === 'Mdx';
   const isMd = node => node.internal.type === 'MarkdownRemark';
-  if (isMdx(node) || isMd(node)) {
-    const slug = createFilePath({ node, getNode });
-    createNodeField({
-      name: 'slug',
-      node,
-      value: slug,
-    });
+  const isPost = node => node.fileAbsolutePath.match(/\/posts\//);
+
+  if ((isMdx(node) || isMd(node)) && isPost(node)) {
+    const slug = '/posts' + createFilePath({ node, getNode });
+    createNodeField({ name: 'type', node, value: 'post' });
+    createNodeField({ name: 'slug', node, value: slug });
   }
 };
 
-exports.createPages = async ({ graphql, actions: { createPage } }) => {
-  const {
-    data: {
-      allMarkdownRemark: { nodes: posts },
-    },
-  } = await graphql(`
+const createPosts = async ({ graphql, actions: { createPage } }) => {
+  const response = await graphql(`
     query {
-      allMarkdownRemark {
+      allMarkdownRemark(filter: { fields: { type: { eq: "post" } } }) {
         nodes {
           fields {
             slug
-          }
-          frontmatter {
-            title
           }
         }
       }
     }
   `);
-
-  const component = path.resolve('./src/templates/post.js');
-
+  const posts = response.data.allMarkdownRemark.nodes;
   posts.forEach(({ fields: { slug } }) => {
     createPage({
       path: slug,
-      component,
+      component: path.resolve('./src/templates/post.js'),
       context: { slug },
     });
   });
 };
+
+exports.createPages = createPosts;
