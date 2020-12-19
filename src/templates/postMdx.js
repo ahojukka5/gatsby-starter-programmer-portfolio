2067d99ddd04d4ae54aac9f4e247f2fdcc6f5be2
Jukka Aho <ahojukka5@gmail.com>
2020-12-19 16:33:01 +0700

    Create template for Mdx

---
+++ src/templates/postMdx.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { MDXProvider } from '@mdx-js/react';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { Link } from 'gatsby';
+
+import SEO from '../components/seo';
+import Layout from '../components/Layout';
+
+const shortcodes = { Link };
+
+const Post = ({ data }) => {
+  const post = data.mdx;
+  return (
+    <Layout>
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <MDXRenderer>{post.body}</MDXRenderer>
+      </div>
+    </Layout>
+  );
+};
+
+export default Post;
+
+export const query = graphql`
+  query($slug: String!) {
+    mdx: mdx(fields: { slug: { eq: $slug } }) {
+      body
+      frontmatter {
+        title
+      }
+      excerpt
+    }
+  }
+`;
