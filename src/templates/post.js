8223670f013291d46d01c47538e163a296c979d7
Jukka Aho <ahojukka5@gmail.com>
2020-12-19 09:44:59 +0700

    Use Layout in post template

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+
 import SEO from '../components/seo';
+import Layout from '../components/Layout';
 
 const Post = ({ data }) => {
   const post = data.markdownRemark;
   return (
-    <div>
+    <Layout>
       <SEO title={post.frontmatter.title} description={post.excerpt} />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
-    </div>
+    </Layout>
   );
 };
 
