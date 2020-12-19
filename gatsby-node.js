3f95d48d8f698acecc4d806cc8ba463517e30139
Jukka Aho <ahojukka5@gmail.com>
2020-12-19 16:32:24 +0700

    Create pages for md and mdx

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,7 +16,15 @@ exports.onCreateNode = ({ node, getNode, actions: { createNodeField } }) => {
 const createPosts = async ({ graphql, actions: { createPage } }) => {
   const response = await graphql(`
     query {
-      allMarkdownRemark(filter: { fields: { type: { eq: "post" } } }) {
+      md: allMarkdownRemark(filter: { fields: { type: { eq: "post" } } }) {
+        nodes {
+          fields {
+            slug
+          }
+        }
+      }
+
+      mdx: allMdx(filter: { fields: { type: { eq: "post" } } }) {
         nodes {
           fields {
             slug
@@ -25,11 +33,22 @@ const createPosts = async ({ graphql, actions: { createPage } }) => {
       }
     }
   `);
-  const posts = response.data.allMarkdownRemark.nodes;
+
+  const posts = response.data.md.nodes;
+  const postsMdx = response.data.mdx.nodes;
+
   posts.forEach(({ fields: { slug } }) => {
     createPage({
       path: slug,
       component: path.resolve('./src/templates/post.js'),
+      context: { slug },
+    });
+  });
+
+  postsMdx.forEach(({ fields: { slug } }) => {
+    createPage({
+      path: slug,
+      component: path.resolve('./src/templates/postMdx.js'),
       context: { slug },
     });
   });
