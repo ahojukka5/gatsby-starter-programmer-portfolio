7792b548e849423026bb736edeabd9d079b26926
Jukka Aho <ahojukka5@gmail.com>
2020-12-20 03:47:40 +0700

    Remove modifications to theme

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,8 +1,11 @@
+import theme from '@theme-ui/preset-deep';
+
 export default {
+  ...theme,
   colors: {
     text: '#d4d4d4',
     background: '#1e1e1e',
-    primary: '#0cf',
+    primary: '#2367c5',
     modes: {
       light: {
         text: '#000',
@@ -11,20 +14,7 @@ export default {
       },
     },
   },
-  fonts: {
-    body: 'system-ui, sans-serif',
-    heading: 'system-ui, sans-serif',
-    monospace: 'Menlo, monospace',
+  styles: {
+    ...theme,
   },
-  fontWeights: {
-    body: 400,
-    heading: 700,
-    bold: 700,
-  },
-  lineHeights: {
-    body: 1.5,
-    heading: 1.125,
-  },
-  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
-  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
 };
