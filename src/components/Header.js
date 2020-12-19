d551a025a52a7682f2769413c4a114e71fa63bb0
Jukka Aho <ahojukka5@gmail.com>
2020-12-20 03:48:22 +0700

    Initial implementation of NavBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,44 @@
-import { Link } from 'gatsby';
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
 import React from 'react';
-import { useColorMode } from 'theme-ui';
+import { Link } from 'gatsby';
+import { useColorMode, Flex, NavLink } from 'theme-ui';
 
-const Header = ({ siteTitle, siteDescription }) => {
+const ColorChangeSwitch = () => {
   const [colorMode, setColorMode] = useColorMode();
   return (
+    <button
+      onClick={e => {
+        setColorMode(colorMode === 'default' ? 'light' : 'default');
+      }}
+    >
+      Toggle {colorMode === 'default' ? 'Light' : 'Dark'}
+    </button>
+  );
+};
+
+const Header = ({ siteTitle, siteDescription }) => {
+  return (
     <header>
-      <button
-        onClick={e => {
-          setColorMode(colorMode === 'default' ? 'light' : 'default');
-        }}
-      >
-        Toggle {colorMode === 'default' ? 'Light' : 'Dark'}
-      </button>
-      <Link to="/">
-        <h1>{siteTitle}</h1>
-        <p>{siteDescription}</p>
-      </Link>
+      <Flex as="nav">
+        <NavLink to="/" as={Link}>
+          {siteTitle}
+        </NavLink>
+        <div sx={{ mx: 'auto' }} />
+        <NavLink to="/posts" as={Link}>
+          Posts
+        </NavLink>
+        <NavLink to="/projects" as={Link}>
+          Projects
+        </NavLink>
+        <NavLink to="/research" as={Link}>
+          Research
+        </NavLink>
+        <NavLink to="/cv" as={Link}>
+          CV
+        </NavLink>
+        <ColorChangeSwitch />
+      </Flex>
     </header>
   );
 };
