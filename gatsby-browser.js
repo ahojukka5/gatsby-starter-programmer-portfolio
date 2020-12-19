4234bf58032c6020f57c98ac7f7d72fce88700a4
Jukka Aho <ahojukka5@gmail.com>
2020-12-19 23:35:54 +0700

    Add shortcode for Link

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
+import { Link } from 'gatsby';
 
 import CodeBlock from './src/components/CodeBlock';
 
-const component = {
+const shortcodes = { Link };
+
+const components = {
   pre: CodeBlock,
+  Link,
 };
 
 export const wrapRootElement = ({ element }) => {
-  return <MDXProvider components={component}>{element}</MDXProvider>;
+  return <MDXProvider components={components}>{element}</MDXProvider>;
 };
