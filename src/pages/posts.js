8ad6c8601505b113b2752a5ff0b016c6e7ccbd28
Jukka Aho <ahojukka5@gmail.com>
2020-12-19 23:36:57 +0700

    Remove unused PropTypes

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 
 import Layout from '../components/Layout';
 
