dfe2b5cd574bcf926f03fa3c55859f3e178ab4ad
Jukka Aho <ahojukka5@gmail.com>
2020-12-20 13:49:42 +0700

    Add option to dynamically change theme

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useMediaQuery, CssBaseline } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 
+export const light = {
+  palette: {
+    type: 'light',
+  },
+};
+
+export const dark = {
+  palette: {
+    type: 'dark',
+  },
+};
+
 const Layout = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(true);
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  // console.log('prefersDarkMode? ', prefersDarkMode);
+
+  const theme = React.useMemo(() => createMuiTheme(darkMode ? dark : light), [
+    darkMode,
+  ]);
+
   return (
-    <>
-      <Header />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header
+        darkMode={darkMode}
+        onClickDarkMode={() => setDarkMode(!darkMode)}
+      />
       <Main>{children}</Main>
       <Footer />
-    </>
+    </ThemeProvider>
   );
 };
 
