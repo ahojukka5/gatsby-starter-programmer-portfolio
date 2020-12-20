78befe8af200e970a614e04c64652f0d5edecf65
Jukka Aho <ahojukka5@gmail.com>
2020-12-20 11:29:26 +0700

    Remove Link shortcode from MDXProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
-import { Link } from 'gatsby';
 
 import CodeBlock from './src/components/CodeBlock';
 
-const shortcodes = { Link };
-
 const components = {
   pre: CodeBlock,
-  Link,
 };
 
 export const wrapRootElement = ({ element }) => {
