24fab9fcb47a8ef2b8b1e511e1de41474d23f577
Jukka Aho <ahojukka5@gmail.com>
2020-12-20 19:07:45 +0700

    Define some mdx components

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { MDXProvider } from '@mdx-js/react';
-
+import Typography from '@material-ui/core/Typography';
 import CodeBlock from './src/components/CodeBlock';
 
 const components = {
+  h1: (() => {
+    const H1 = props => <Typography {...props} component="h1" variant="h1" />;
+    return memo(H1);
+  })(),
+  h2: (() => {
+    const H2 = props => <Typography {...props} component="h2" variant="h2" />;
+    return memo(H2);
+  })(),
   pre: CodeBlock,
 };
 
