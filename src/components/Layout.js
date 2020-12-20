2105e8aee48c2b488f0c6f7d1a0bd525cfff37e7
Jukka Aho <ahojukka5@gmail.com>
2020-12-20 12:59:18 +0700

    Remove SEO from Layout temporarily

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import SEO from '../components/seo';
-import Container from '@material-ui/core/Container';
 
 import Header from './Header';
 import Main from './Main';
@@ -8,13 +6,11 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
-    <Container>
-      <SEO title="Jukka Aho" />
-      <SEO title="ahojukka5" />
+    <>
       <Header />
       <Main>{children}</Main>
       <Footer />
-    </Container>
+    </>
   );
 };
 
