aba27c0a8fc3a8b838c2724b496d976879cbda22
Jukka Aho <ahojukka5@gmail.com>
2020-12-20 13:00:09 +0700

    First version of AppBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link } from 'gatsby-theme-material-ui';
+
+import {
+  AppBar,
+  Container,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Toolbar,
+} from '@material-ui/core';
+import { Home } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
 
 import useSiteMetadata from '../hooks/useSiteMetadata';
 
@@ -16,26 +28,57 @@ const ColorChangeSwitch = () => {
   );
 };
 
+const useStyles = makeStyles({
+  navDisplayFlex: {
+    display: `flex`,
+    justifyContent: `space-between`,
+  },
+  navbarDisplayFlex: {
+    display: `flex`,
+    justifyContent: `space-between`,
+  },
+  linkText: {
+    textDecoration: `none`,
+    textTransform: `uppercase`,
+    color: `white`,
+  },
+});
+
+const navLinks = [
+  { title: `About me`, path: `/about` },
+  { title: `Posts`, path: `/posts` },
+  { title: `Projects`, path: `/projects` },
+  { title: `Research`, path: `/research` },
+  { title: `CV`, path: `/cv` },
+];
+
 const Header = () => {
-  const { title } = useSiteMetadata();
+  const { author, title, description } = useSiteMetadata();
+  const classes = useStyles();
+
   return (
-    <header>
-        <Link to="/">
-          {title}
-        </Link>
-        <Link to="/posts">
-          Posts
-        </Link>
-        <Link to="/projects">
-          Projects
-        </Link>
-        <Link to="/research">
-          Research
-        </Link>
-        <Link to="/cv">
-          CV
-        </Link>
-    </header>
+    <AppBar position="static">
+      <Toolbar>
+        <Container className={classes.navbarDisplayFlex}>
+          <IconButton edge="start" color="inherit" aria-label="home">
+            <Home fontSize="large" />
+          </IconButton>
+          <List
+            component="nav"
+            aria-labelledby="main navigation"
+            className={classes.navDisplayFlex}
+          >
+            {navLinks.map(({ title, path }) => (
+              <a href={path} key={title} className={classes.linkText}>
+                <ListItem button>
+                  <ListItemText primary={title} />
+                </ListItem>
+              </a>
+            ))}
+          </List>
+        </Container>
+      </Toolbar>
+    </AppBar>
   );
 };
 
