86ccf3f7ac084ab51c483263a607271140ba676c
Jukka Aho <ahojukka5@gmail.com>
2020-12-21 15:30:45 +0700

    Updates

---
+++ component---src-pages-index-js-efdc678c2619f006948f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RXBc:function(n,e,a){"use strict";a.r(e);var o=a("q1tI"),t=a.n(o),u=a("7oih");e.default=function(){return t.a.createElement(u.a,null,"Hello, world! My name is Jukka.")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-efdc678c2619f006948f.js.map
